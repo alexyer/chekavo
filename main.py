299c19beb77a8034d34d6a4fb816e90d0f7ae4d6
Alexander Yermakov <Alexander.Yermakov@dataart.com>
2017-09-11 12:21:22 +0300

    Add additional commands

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import re
+from datetime import datetime
 
 import facebook
 import logging
 from sqlalchemy import create_engine, exists
 from sqlalchemy.orm import sessionmaker, scoped_session
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
-from models import Base, Event
+from models import Base, Event, Application
 
 graph = facebook.GraphAPI(access_token=os.environ['CHEKAVO_FB_TOKEN'])
 engine = create_engine(os.environ['CHEKAVO_DB'])
@@ -15,10 +16,9 @@ engine = create_engine(os.environ['CHEKAVO_DB'])
 Base.metadata.create_all(engine)
 Session = scoped_session(sessionmaker(bind=engine))
 
-
 logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
 
 
 def parse_event(bot, update):
@@ -40,10 +40,71 @@ def parse_event(bot, update):
         session.commit()
 
         bot.send_message(chat_id=update.message.chat_id,
-                         text='New event: [{}]({}), id: {}'.format(new_event.name,
-                                                                   new_event.url,
-                                                                   new_event.id),
+                         text='New event: [{}]({}), id: {}'.format(
+                             new_event.name,
+                             new_event.url,
+                             new_event.id),
                          parse_mode='Markdown')
+
+
+def upcoming(bot, update):
+    session = Session()
+    upcoming_events = ('{}: [{}]({}) at {}'.format(e.id,
+                                                   e.name,
+                                                   e.url,
+                                                   e.start_date.strftime('%b %d %a %H:%M'))
+                       for e in session.query(Event).
+                           filter(Event.start_date >= datetime.now()).
+                           order_by(Event.start_date))
+
+    bot.send_message(chat_id=update.message.chat_id,
+                     text='Upcoming events:\n{}'.format('\n'.join(upcoming_events)),
+                     parse_mode='Markdown')
+
+
+def apply(bot, update, args):
+    session = Session()
+    event = _get_event(args, session)
+    username = update.message.from_user.username
+
+    if not session.query(exists().where(Application.event_id == event.id).
+                                  where(Application.username == username)).scalar():
+        new_application = Application(event_id=event.id, username=username)
+        session.add(new_application)
+        session.commit()
+
+        bot.send_message(chat_id=update.message.chat_id,
+                         text='@{} is going to [{}]({})'.format(username,
+                                                                event.name,
+                                                                event.url),
+                         parse_mode='Markdown')
+
+
+def _get_event(args, session):
+    return session.query(Event).filter(Event.id == args[0]).first()
+
+
+def who(bot, update, args):
+    session = Session()
+    event = _get_event(args, session)
+    people = ('@{}'.format(a.username) for a in event.applications)
+    bot.send_message(chat_id=update.message.chat_id,
+                     text='[{}]({}):\n{}'.format(event.name, event.url,
+                                                 '\n'.join(people)),
+                     parse_mode='Markdown')
+
+
+def bail(bot, update, args):
+    session = Session()
+    event = _get_event(args, session)
+    username = update.message.from_user.username
+    session.query(Application).filter(Application.event_id == event.id,
+                                      Application.username == username).delete()
+    bot.send_message(chat_id=update.message.chat_id,
+                     text='@{} is not going to [{}]({})'.format(username,
+                                                                event.name,
+                                                                event.url),
+                     parse_mode='Markdown')
 
 
 def main():
@@ -57,6 +118,18 @@ def main():
 
     parse_event_handler = MessageHandler(Filters.text, parse_event)
     dispatcher.add_handler(parse_event_handler)
+
+    upcoming_handler = CommandHandler('upcoming', upcoming)
+    dispatcher.add_handler(upcoming_handler)
+
+    apply_handler = CommandHandler('apply', apply, pass_args=True)
+    dispatcher.add_handler(apply_handler)
+
+    who_handler = CommandHandler('who', who, pass_args=True)
+    dispatcher.add_handler(who_handler)
+
+    bail_handler = CommandHandler('bail', bail, pass_args=True)
+    dispatcher.add_handler(bail_handler)
 
     updater.start_polling()
 
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -10,4 +11,16 @@ class Event(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     url = Column(String)
-    start_date = Column(DateTime)
+    start_date = Column(DateTime, index=True)
+
+    applications = relationship('Application', back_populates='event')
+
+
+class Application(Base):
+    __tablename__ = 'applications'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    event_id = Column(Integer, ForeignKey('events.id'))
+
+    event = relationship('Event', back_populates='applications')
